9f82637224992a459d841eaf9e7773a6b4587165
ahprinters <ahps323@gmail.com>
2024-10-20 16:23:26 +0600

    strpos

---
+++ strpos.php
@@ -0,0 +1,18 @@
+
+<?php   
+//   strpos — Find the position of the first occurrence of a substring in a string
+
+// Find the numeric position of the first occurrence of needle in the haystack string.
+
+$mystring = 'abc';
+$findme   = 'a';
+$pos = strpos($mystring, $findme);
+
+// Note our use of ===.  Simply == would not work as expected
+// because the position of 'a' was the 0th (first) character.
+if ($pos === false) {
+    echo "The string '$findme' was not found in the string '$mystring'";
+} else {
+    echo "The string '$findme' was found in the string '$mystring'";
+    echo " and exists at position $pos";
+}
